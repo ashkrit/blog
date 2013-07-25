19f55a66c1554b2f85d14c93a78c2133049c1c37
Ashkrit <ashkrit@gmail.com>
2013-07-26 02:27:58 +0800

    Create ObjectType.java

---
+++ allocation/ObjectType.java
@@ -0,0 +1,12 @@
+
+
+public interface ObjectType {
+  public void navigate(int index);
+	public void setInt(int value);
+	public void setLong(long value);
+	public void setByte(byte value);
+	
+	public int getInt();
+	public long getLong();
+	public byte getByte();
+}
