8bb11cae71c66b89b302e41b7ec2cecf8d86cf82
Ashkrit <ashkrit@gmail.com>
2013-07-26 02:25:28 +0800

    Create Allocator.java

---
+++ allocation/Allocator.java
@@ -0,0 +1,24 @@
+
+public class Allocator {
+  public enum Type{HEAP,BB,DBB,OFFHEAP}
+	public static ObjectType allocate(Type type , int items)
+	{
+		if(type.equals(Type.HEAP))
+		{
+			return new HeapAllocatedObject(items);
+		}
+		else if(type.equals(Type.OFFHEAP))
+		{
+			return new OffHeapObject(items);
+		}
+		else if(type.equals(Type.DBB))
+		{
+			return new DirectByteBufferObject(items);
+		}
+		else if(type.equals(Type.BB))
+		{
+			return new ByteBufferObject(items);
+		}
+		return null;
+	}
+}
