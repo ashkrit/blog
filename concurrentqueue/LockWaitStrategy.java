0a23a00f1459ff27de000e7166c0a50f026db39d
Ashkrit <ashkrit@gmail.com>
2013-10-15 17:18:43 +0800

    Create LockWaitStrategy.java

---
+++ concurrentqueue/LockWaitStrategy.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockWaitStrategy implements WaitStrategy {
+
+	private ReentrantLock lock = new ReentrantLock();
+	private Condition dataChanged = lock.newCondition();
+	
+	@Override
+	public void block() throws InterruptedException {
+		try {
+			lock.lock();
+			dataChanged.await();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	@Override
+	public void release() {
+		try {
+			lock.lock();
+			dataChanged.signalAll();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+}
