1a38c518554b0cddf1703a7aa08a01f741c916e2
Ashkrit <ashkrit@gmail.com>
2014-06-28 23:49:46 +0800

    Create MethodHandleMethodInvoker.java

---
+++ reflect/MethodHandleMethodInvoker.java
@@ -0,0 +1,20 @@
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+
+
+public class MethodHandleMethodInvoker implements IOrderMethodInvoker {
+
+	private MethodHandle m;
+	public MethodHandleMethodInvoker(String methodName) throws Exception
+	{
+		MethodHandles.Lookup lookup = MethodHandles.lookup();
+		m = lookup.findVirtual(Order.class, methodName,MethodType.methodType(Integer.TYPE));
+	}
+	
+	@Override
+	public int integerGetMethod(Order order) throws Throwable {
+		return (int) m.invoke(order);
+	}	
+}
